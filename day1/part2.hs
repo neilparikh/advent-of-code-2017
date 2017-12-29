7e76f54d839aa0240ce9efea6c7128abb20c2426
Neil Parikh <parikh.neil@me.com>
2017-12-28 19:46:21 -0500

    use cycle from prelude instead writing my own

--- day1/part2.hs
+++ day1/part2.hs
@@ -1,15 +1,9 @@
-applyNTimes :: Int -> (a -> a) -> a -> a
-applyNTimes n f = foldl (.) id (replicate n f)
-
-cycleOnce :: String -> String
-cycleOnce "" = ""
-cycleOnce (x:xs) = xs ++ [x]
-
 captcha :: String -> Int
 captcha "" = 0
 captcha lst = sum . map (read . (:[]) . fst) . filter (\(a, b) -> a == b) $ zip lst newLst
     where
-    newLst = applyNTimes ((length lst) `div` 2) cycleOnce lst
+    len = length lst
+    newLst = take len . drop (len `div` 2) . cycle $ lst
 
 main = do
     input <- getLine
